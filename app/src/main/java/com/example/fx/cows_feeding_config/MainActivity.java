e7b0c118a05a672e5dcb8e2776560836dc8f251a
fx109138 <1091381572@qq.com>
2017-09-11 18:33:45 +0800

    完成页面间跳转

--- app/src/main/java/com/example/fx/cows_feeding_config/MainActivity.java
+++ app/src/main/java/com/example/fx/cows_feeding_config/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.fx.cows_feeding_config;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private android.widget.Button data;
+    private android.widget.Button need;
+    private android.widget.Button optimize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+    }
+
+    private void initView() {
+        this.optimize = (Button) findViewById(R.id.optimize);
+        this.need = (Button) findViewById(R.id.need);
+        this.data = (Button) findViewById(R.id.data);
+    }
+
+    private void initEvent() {
+        data.setOnClickListener(this);
+        need.setOnClickListener(this);
+        optimize.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.data:
+                startActivity(new Intent(this, DataActivity.class));
+                break;
+            case R.id.need:
+                startActivity(new Intent(this, NeedActivity.class));
+                break;
+            case R.id.optimize:
+                startActivity(new Intent(this, OptimizeActivity.class));
+                break;
+        }
     }
 }
