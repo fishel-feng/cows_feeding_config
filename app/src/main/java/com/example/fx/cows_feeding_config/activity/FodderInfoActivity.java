14e40c1ea21cd7fcd460a6ccc5c6dfc7ab80de3d
fx109138 <1091381572@qq.com>
2017-09-12 14:23:43 +0800

    隐藏标题，添加表头

--- app/src/main/java/com/example/fx/cows_feeding_config/activity/FodderInfoActivity.java
+++ app/src/main/java/com/example/fx/cows_feeding_config/activity/FodderInfoActivity.java
@@ -1,9 +1,8 @@
 package com.example.fx.cows_feeding_config.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.fx.cows_feeding_config.R;
 import com.example.fx.cows_feeding_config.entity.Fodder;
@@ -16,7 +15,7 @@ public class FodderInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fodder_info);
         Intent intent=getIntent();
         Fodder fodder=intent.getParcelableExtra("fodder");
-        TextView textView= (TextView) findViewById(R.id.aaaaa);
-        textView.setText(fodder.getName());
+//        TextView textView= (TextView) findViewById(R.id.aaaaa);
+//        textView.setText(fodder.getName());
     }
 }
