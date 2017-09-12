1f35181d240d53718cb6ef39d209b1891282285b
fx109138 <1091381572@qq.com>
2017-09-12 22:15:16 +0800

    完成数据回显

--- app/src/main/java/com/example/fx/cows_feeding_config/activity/FodderInfoActivity.java
+++ app/src/main/java/com/example/fx/cows_feeding_config/activity/FodderInfoActivity.java
@@ -1,21 +1,57 @@
 package com.example.fx.cows_feeding_config.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.fx.cows_feeding_config.R;
 import com.example.fx.cows_feeding_config.entity.Fodder;
 
 public class FodderInfoActivity extends AppCompatActivity {
 
+    private android.widget.EditText etName;
+    private android.widget.Spinner spType;
+    private android.widget.EditText etDryMatter;
+    private android.widget.EditText etCalcium;
+    private android.widget.EditText etPhosphorus;
+    private android.widget.EditText etEnergy;
+    private android.widget.EditText etCrudeProtein;
+    private android.widget.EditText etPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fodder_info);
-        Intent intent=getIntent();
-        Fodder fodder=intent.getParcelableExtra("fodder");
-//        TextView textView= (TextView) findViewById(R.id.aaaaa);
-//        textView.setText(fodder.getName());
+        initView();
+        initData();
+        initEvent();
+    }
+
+    private void initView() {
+        this.etPrice = (EditText) findViewById(R.id.et_price);
+        this.etCrudeProtein = (EditText) findViewById(R.id.et_crude_protein);
+        this.etEnergy = (EditText) findViewById(R.id.et_energy);
+        this.etPhosphorus = (EditText) findViewById(R.id.et_phosphorus);
+        this.etCalcium = (EditText) findViewById(R.id.et_calcium);
+        this.etDryMatter = (EditText) findViewById(R.id.et_dry_matter);
+        this.spType = (Spinner) findViewById(R.id.sp_type);
+        this.etName = (EditText) findViewById(R.id.et_name);
+    }
+
+    private void initData() {
+        Fodder fodder = getIntent().getParcelableExtra("fodder");
+        etName.setText(fodder.getName());
+        spType.setSelection(fodder.getType() - 1);
+        etDryMatter.setText(String.valueOf(fodder.getDryMatter()));
+        etCalcium.setText(String.valueOf(fodder.getCalcium()));
+        etPhosphorus.setText(String.valueOf(fodder.getPhosphorus()));
+        etEnergy.setText(String.valueOf(fodder.getEnergy()));
+        etCrudeProtein.setText(String.valueOf(fodder.getCrudeProtein()));
+        etPrice.setText(String.valueOf(fodder.getPrice()));
+    }
+
+    private void initEvent() {
+
     }
 }
