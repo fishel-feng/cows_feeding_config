7965a4fb7ddd020558218558345f12c971ea88e7
fx109138 <1091381572@qq.com>
2017-09-16 18:53:08 +0800

    修正了json

--- app/src/main/java/com/example/fx/cows_feeding_config/util/ObjectToJsonUtil.java
+++ app/src/main/java/com/example/fx/cows_feeding_config/util/ObjectToJsonUtil.java
@@ -21,32 +21,32 @@ public class ObjectToJsonUtil {
         JSONObject cowObject = new JSONObject();
         result.put("fodder", fodderArray);
         result.put("cow", cowObject);
-        result.put("coarse", coarse);
-        result.put("concentrate", concentrate);
+        result.put("coarse", String.valueOf(coarse));
+        result.put("concentrate", String.valueOf(concentrate));
         for (int i = 0; i < fodders.size(); i++) {
             JSONObject fodderObject = new JSONObject();
             Fodder fodder = fodders.get(i);
             fodderObject.put("name", fodder.getName());
-            fodderObject.put("type", fodder.getType());
-            fodderObject.put("dry_matter", fodder.getDryMatter());
-            fodderObject.put("calcium", fodder.getCalcium());
-            fodderObject.put("phosphorus", fodder.getPhosphorus());
-            fodderObject.put("energy", fodder.getEnergy());
-            fodderObject.put("crude_protein", fodder.getCrudeProtein());
-            fodderObject.put("price", fodder.getPrice());
-            fodderObject.put("variable", fodder.isVariable());
-            fodderObject.put("dosage", fodder.getDosage());
-            fodderObject.put("max_dosage", fodder.getMaxDosage());
-            fodderObject.put("min_dosage", fodder.getMinDosage());
+            fodderObject.put("type", String.valueOf(fodder.getType()));
+            fodderObject.put("dry_matter", String.valueOf(fodder.getDryMatter()));
+            fodderObject.put("calcium", String.valueOf(fodder.getCalcium()));
+            fodderObject.put("phosphorus", String.valueOf(fodder.getPhosphorus()));
+            fodderObject.put("energy", String.valueOf(fodder.getEnergy()));
+            fodderObject.put("crude_protein", String.valueOf(fodder.getCrudeProtein()));
+            fodderObject.put("price", String.valueOf(fodder.getPrice()));
+            fodderObject.put("variable", String.valueOf(fodder.isVariable()));
+            fodderObject.put("dosage", String.valueOf(fodder.getDosage()));
+            fodderObject.put("max_dosage", String.valueOf(fodder.getMaxDosage()));
+            fodderObject.put("min_dosage", String.valueOf(fodder.getMinDosage()));
             fodderArray.put(fodderObject);
         }
         cowObject.put("variety", cow.getVariety());
-        cowObject.put("weight", cow.getWeight());
-        cowObject.put("weight_change", cow.getWeightChange());
-        cowObject.put("milk_production", cow.getMilkProduction());
-        cowObject.put("milk_fat", cow.getMilkFat());
-        cowObject.put("milk_protein", cow.getMilkProtein());
-        cowObject.put("lactation_weeks", cow.getLactationWeeks());
+        cowObject.put("weight", String.valueOf(cow.getWeight()));
+        cowObject.put("weight_change", String.valueOf(cow.getWeightChange()));
+        cowObject.put("milk_production", String.valueOf(cow.getMilkProduction()));
+        cowObject.put("milk_fat", String.valueOf(cow.getMilkFat()));
+        cowObject.put("milk_protein", String.valueOf(cow.getMilkProtein()));
+        cowObject.put("lactation_weeks", String.valueOf(cow.getLactationWeeks()));
         return result;
     }
 }
