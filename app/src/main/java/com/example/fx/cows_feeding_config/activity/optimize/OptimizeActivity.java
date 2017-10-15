5934457b8f144e549a7f4b2cba325b6bf04e2483
fx109138 <1091381572@qq.com>
2017-10-15 20:04:24 +0800

    ..

--- app/src/main/java/com/example/fx/cows_feeding_config/activity/optimize/OptimizeActivity.java
+++ app/src/main/java/com/example/fx/cows_feeding_config/activity/optimize/OptimizeActivity.java
@@ -250,6 +250,8 @@ public class OptimizeActivity extends AppCompatActivity implements View.OnClickL
                     });
                 }
                 break;
+            default:
+                break;
         }
     }
 
